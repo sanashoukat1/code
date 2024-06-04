adf238fe98127fbb62c44b9d38d4a6a5bd0779b9
sanashoukat1 <sanashoukat189@gmail.com>
2024-06-04 23:11:35 +0500

    band name

---
+++ me.py
@@ -0,0 +1,5 @@
+print("Welcomoe to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n") 
+print("Your band mane could be " + street + " " + pet) 
+vehari             
