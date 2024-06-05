a42cbc060e1e74415c3c7b28704146856ee80f8b
sanashoukat1 <sanashoukat189@gmail.com>
2024-06-05 18:59:58 +0500

    str 2

--- str.py
+++ str.py
@@ -1,3 +1,5 @@
-num_char = len(input("What is your name?"))
-new_num_char = str(num_char)
-print("Your name has " + new_num_char + " characters.")
+print(70 + float("100.5"))
+print(str(123) + str(234))
+
+a = float("100.5")
+print(type(a))
