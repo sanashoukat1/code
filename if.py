48683fc00dd9d301749e51d22dbc855885abef60
sanashoukat1 <sanashoukat189@gmail.com>
2024-06-10 16:30:00 +0500

    elif statement

--- if.py
+++ if.py
@@ -1,7 +1,16 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm?"))
 
-if height > 120:
+if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))    
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    elif age < 22:
+        print("Pleaase pay $10.")    
+    else:
+        print("Please pay $12.")        
 else:
     print("Sorry, you have to grow taller before you can ride.")    
